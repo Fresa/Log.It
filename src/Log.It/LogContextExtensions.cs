66100ec9aa35a060ffd79616a3331b7c6d9d0279
Fredrik Arvidsson <fresa@fresa.se>
2018-07-18 19:31:35 +0200

    add convenient extension methods for capturing logical thread context

---
+++ src/Log.It/LogContextExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Log.It
+{
+    public static partial class LogContextExtensions
+    {
+        public static IDisposable Capture<T>(this ILogContext logContext, string key, T value)
+        {
+            logContext.Set(key, value);
+            return new DisposeAction(() => logContext.Remove(key));
+        }
+    }
+}
---
+++ src/Log.It/LoggerExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Log.It
+{
+    public static partial class LoggerExtensions
+    {
+        public static IDisposable CaptureLogicalThreadContext<T>(this ILogger logger, string key, T value)
+        {
+            return logger.LogicalThread.Capture(key, value);
+        }
+    }
+}
